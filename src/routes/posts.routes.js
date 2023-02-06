ca0a65097efd38f7e5ca2356b2f3bd743136a22c
ManuelaDiSalvo <manueladisalvo123@gmail.com>
2023-02-06 18:41:29 +0100

    eliminar moderator y admin

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -3,18 +3,18 @@ const router = Router()
 
 import * as postsCtrl from '../controllers/posts.controllers'
 import { authJwt } from "../middlewares";
-import { isAdmin, isModerator, verifyToken } from "../middlewares/authJwt";
+import {verifyToken } from "../middlewares/authJwt";
 
 
 // Ruta para crear un post
-router.post('/', [authJwt.verifyToken, authJwt.isModerator], postsCtrl.createPost)
+router.post('/', [authJwt.verifyToken], postsCtrl.createPost)
 // Ruta para obtener un producto
 router.get('/', postsCtrl.getPosts)
 // Ruta para obtener un solo post por id
 router.get('/:postId', postsCtrl.getPostById)
 // Ruta para actualizar un post
-router.put('/:postId', [authJwt.verifyToken, authJwt.isAdmin], postsCtrl.updatePostById)
+router.put('/:postId', [authJwt.verifyToken], postsCtrl.updatePostById)
 // Ruta para borrar un post
-router.delete('/:postId', [authJwt.verifyToken, authJwt.isAdmin], postsCtrl.deletePostById)
+router.delete('/:postId', [authJwt.verifyToken], postsCtrl.deletePostById)
 
 export default router;
